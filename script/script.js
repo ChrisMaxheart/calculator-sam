a522c4d4a1fe1a0cbf249eb040090e2ee3f8c1e2
Samuel Henry Kurniawan <samuelhenrykurniawan@yahoo.com>
2018-06-04 09:14:56 +0800

    finalized calculator with jquery

--- script/script.js
+++ script/script.js
@@ -2,7 +2,8 @@ let lst = document.getElementsByClassName("button");
 
 Array.from(lst).forEach(
   function(x) {
-    x.onclick = function () {buttonPress(x.textContent)};
+    x.onclick = function () {buttonPress(x.innerHTML)};
+    // console.log(x.innerHTML);
   }
 );
 
@@ -13,4 +14,76 @@ function buttonPress(symbol) {
   document.getElementById("display").innerHTML = statement;
 }
 
-// need to add: eval, try except, undo, clear, keyboard support
+function calcResult() {
+  // console.log(statement);
+  try {
+    if (statement == "") {
+      throw(err);
+    }
+    statement = eval(statement);
+    if (statement == "Infinity") {
+      throw(err);
+    } 
+  } catch(err) {
+    if (statement == "Infinity") {
+      statement = "Zero Div ERROR";
+    } else {
+      statement = "ERROR";
+    }
+  }
+  document.getElementById("display").innerHTML = statement;
+  statement = "";
+}
+
+Array.from(document.getElementsByClassName("equal")).forEach(
+  function(x) {
+    x.onclick = function() {
+      calcResult();
+    }
+  })
+
+$(".clear").click(function() {
+  erase();
+});
+
+$(".undo").click(function() {
+  undo();
+});
+
+function erase() {
+  statement = "";
+  $("#display").html(statement);
+}
+
+function undo() {
+  if(statement.length != 0) {
+    statement = statement.slice(0,statement.length-1);
+    $("#display").html(statement);
+  }
+}
+
+$("body").keypress(function(x) {
+  if (x.which >= 48 && x.which <=57)  {
+    buttonPress(x.which-48);
+  } else if (x.which == 47) {
+    buttonPress('/');
+  } else if (x.which == 46) {
+    buttonPress('.');
+  } else if (x.which == 43) {
+    buttonPress('+');
+  } else if (x.which == 45) {
+    buttonPress('-');
+  } else if (x.which == 42) {
+    buttonPress('*');
+  } else if (x.which == 61 || x.which == 13) {
+    calcResult();
+  } else if (x.which == 127) {
+    erase();
+  }
+});
+
+$("body").keydown(function(x) {
+  if (x.which == 8) {
+    undo();
+  }
+});
